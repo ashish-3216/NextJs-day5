a318fab8aff1d1b67654d7b8f4589a7ee35b0192
ashish-3216 <ashish.work.3216@gmail.com>
2025-02-26 14:44:19 +0530

    inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,114 +1,79 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+import { useState } from "react";
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [students, setStudents] = useState([]);
+  const [teachers, setTeachers] = useState([]);
+  const [findTeacher, setFindTeacher] = useState([]);
+  const [Id, setTeacherId] = useState(0);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const fetchStudentData = async () => {
+    const res = await fetch("/api/student");
+    const data = await res.json();
+    return data;
+  };
+  const fetchTeacherData = async () => {
+    const res = await fetch("/api/teachers");
+    const data = await res.json();
+    return data;
+  };
+  const findStudents = (id) => {
+    if (id > 5) {
+      return [];
+    }
+    const student = students.filter((student) => {
+      return student.teacherId === id;
+    });
+    return student;
+  };
+  return (
+    <div className="container">
+      <div className="student-container">
+        <button
+          onClick={async () => {
+            const data = await fetchStudentData();
+            setStudents(data);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Fetch Students Data
+        </button>
+        {students.map((student) => {
+          return <div key={student.id}>{student.name}</div>;
+        })}
+      </div>
+      <div className="teacher-container">
+        <button
+          onClick={async () => {
+            const data = await fetchTeacherData();
+            setTeachers(data);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Fetch Teachers Data
+        </button>
+        {teachers.map((teacher) => {
+          return <div key={teacher.id}>{teacher.name}</div>;
+        })}
+      </div>
+      <div className="find-container">
+        <input
+          type="number"
+          placeholder="Enter Teacher ID"
+          onChange={(e) => {
+            setTeacherId(parseInt(e.target.value));
+          }}
+        />
+        <button
+          onClick={() => {
+            setFindTeacher(findStudents(Id));
+            setTeacherId(0);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Find Students
+        </button>
+        {Id > 5 && <div>Invalid Teacher ID</div>}
+        {findTeacher.length > 0 &&
+          findTeacher.map((student) => {
+            return <div key={student.id}>{student.name}</div>;
+          })}
+      </div>
     </div>
   );
 }
